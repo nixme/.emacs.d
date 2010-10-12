133788d34983036efcd5672f707e211db147d321
Gopal Patel <nixme@stillhope.com>
2010-10-12 11:47:42 -0700

    Display ido results vertically.

--- other.el
+++ other.el
@@ -81,6 +81,16 @@
   ido-enable-flex-matching t
   ido-create-new-buffer 'always
   ido-max-prospects 8)
+;; display ido results vertically, rather than horizontally
+;;   from tipcharper, jpkotta: http://emacswiki.org/emacs/InteractivelyDoThings
+(setq ido-decorations
+      (quote
+       ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]"
+        " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation ()
+  (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
+
 
 ;; sr-speedbar
 (setq speedbar-show-unknown-files t)
