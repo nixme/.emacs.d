3ca72f66ea8fcd890fd8844e2d71ba07c47028e7
Gopal Patel <nixme@stillhope.com>
2010-12-09 15:33:02 -0800

    Better speedbar font and styling.

--- init-speedbar.el
+++ init-speedbar.el
@@ -16,4 +16,21 @@
 ;; disable line numbers in the speedbar frame
 (add-to-list 'linum-disabled-modes-list '(speedbar-mode))
 
+;; nicer fonts for speedbar when in GUI
+(when (window-system)
+  ;; keep monospace buttons, but smaller height
+  (set-face-attribute 'speedbar-button-face nil :height 100)
+
+  ;; change to system default UI font for entries
+  (dolist (face (list 'speedbar-file-face 'speedbar-directory-face
+                      'speedbar-tag-face  'speedbar-selected-face
+                      'speedbar-highlight-face))
+    (if (eq system-type 'darwin)  ;; Lucida Grande on OS X
+        (set-face-attribute face nil :family "Lucida Grande" :height 110)
+      (set-face-attribute face nil :height 110))))
+
+;; no left fringe and half-size right fringe. TODO: doesn't work
+(add-to-list 'speedbar-frame-parameters '(left-fringe . 0))
+
+
 (provide 'init-speedbar)
