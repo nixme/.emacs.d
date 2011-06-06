7ccdba66d9d1b56a1067ece5d76108bb0a4f3259
Gopal Patel <nixme@stillhope.com>
2011-06-05 21:04:27 -0700

    Trigger ruby-mode for Guardfiles

--- init-ruby.el
+++ init-ruby.el
@@ -24,6 +24,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 
 ;; unset ruby-electric keybindings for certain paired characters
 ;;   since autopair is much superior for them
