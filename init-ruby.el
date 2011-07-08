e215c47b556a19b39f56a0d811773ce3ab39a41b
Gopal Patel <nixme@stillhope.com>
2011-07-08 15:07:36 -0700

    ruby-mode for *.rabl

--- init-ruby.el
+++ init-ruby.el
@@ -20,6 +20,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
